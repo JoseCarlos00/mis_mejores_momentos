bdb76848fe9eab9361240a14768b05e1c5389df3
JoseCarlos00 <101861240+JoseCarlos00@users.noreply.github.com>
2025-11-17 11:50:01 -0600

    Feat: se agrego una function principal para inicializar la gallery

--- index.js
+++ index.js
@@ -1,30 +1,52 @@
-import { nameFiles, basePath } from "./const.js";
+import { getImages } from "./getImages.js";
 
+// Elementos del DOM
 const containerGallery = document.querySelector('.container-gallery');
+const modalSlider = document.querySelector('#modal-slider'); 
+const slidesContainer = document.querySelector('.slides');
 
-
-nameFiles.forEach(({ name, desc}) => {
-	const img = new Image();
-	img.src = `${basePath}${name}`;
-  img.alt = desc;
-	img.loading = 'lazy';
-
-	img.onload = () => containerGallery.appendChild(img);
-  img.onclick = () => openFulImg(img.src);
-});
-
-const fulImgBox = document.getElementById('fulImgBox') 
-const fulImg = document.getElementById('fulImg');
 const btnClosed = document.querySelector('.btn-closed');
+const buttonPrev = document.querySelector('.slider-btn-left');
+const buttonNext = document.querySelector('.slider-btn-right');
 
-btnClosed.addEventListener('click', closeImg);
+
+const images = getImages();
+let currentIndex = 0;
+
+// Eventos
+btnClosed.addEventListener('click', closedModal);
+buttonNext.addEventListener('click', () => showSlide(currentIndex + 1));
+buttonPrev.addEventListener('click', () => showSlide(currentIndex - 1));
 
 
-function openFulImg(reference) {
-	fulImgBox.style.display = 'flex';
-	fulImg.src = reference;
+function openModal(index) {
+	currentIndex = index;
+	modalSlider.style.display = 'flex';
+	showSlide(currentIndex);
 }
 
-function closeImg() {
-	fulImgBox.style.display = 'none';
+function closedModal() {
+	modalSlider.style.display = 'none';
 }
+
+function showSlide(index) {
+	const totalSlides = images.length;
+	currentIndex = (index + totalSlides) % totalSlides;
+	slidesContainer.style.transform = `translateX(${-currentIndex * 100}%)`;
+}
+
+function initializeGallery() {
+	containerGallery.innerHTML = '';
+	slidesContainer.innerHTML = '';
+
+	images.forEach((img, index) => {
+		// Añade la imagen a la galería principal
+		img.onclick = () => openModal(index);
+		containerGallery.appendChild(img);
+
+		// Añade la imagen al carrusel
+		slidesContainer.insertAdjacentHTML('beforeend', `<img class="slider-img" src="${img.src}" alt="${img.alt}">`);
+	});
+}
+
+initializeGallery();
